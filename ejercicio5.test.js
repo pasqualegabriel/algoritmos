eaa5e51fd03acec598cec42cd5ff13b34d63c0ae
Gabriel Pasquale <pasqualeunq@gmail.com>
2020-09-10 19:34:55 -0300

    test ejercicio 5 updated

--- ejercicio5.test.js
+++ ejercicio5.test.js
@@ -11,8 +11,7 @@ test('test 1', () => {
   expect(potencia(2, 5)).toBe(32)
   expect(potencia(2, 6)).toBe(64)
 
-  expect(potenciaN(2, 3)).toBe(14)
+  expect(potenciaN(2, 2)).toBe(6)
   expect(potenciaN(2, 4)).toBe(30)
-  expect(potenciaN(2, 5)).toBe(62)
-  expect(potenciaN(2, 6)).toBe(126)
+  expect(potenciaN(2, 8)).toBe(510)
 })
